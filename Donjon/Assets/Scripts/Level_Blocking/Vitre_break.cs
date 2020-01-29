1917ad80e6978d99d043f5be69488bed2bc7f269
Bryndye <57522753+Bryndye@users.noreply.github.com>
2020-01-29 12:50:22 +0100

    Level blocking started

---
+++ Donjon/Assets/Scripts/Level_Blocking/Vitre_break.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Vitre_break : MonoBehaviour
+{
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.collider.CompareTag("Arrow"))
+        {
+            Destroy(gameObject);
+        }
+    }
+}
