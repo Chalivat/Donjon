ef87ded41f4a221a28a942c6f6d7501eef069e32
Bryndye <57522753+Bryndye@users.noreply.github.com>
2020-02-17 17:56:41 +0100

    porte
    je dois commit

---
+++ Donjon/Assets/Scripts/Trash/Camer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camer : MonoBehaviour
+{
+    public GameObject Cam;
+    public GameObject Player;
+    public GameObject CamMax;
+    public float DistanceMax;
+    public float dd;
+
+    private void Awake()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        var Vec = Cam.transform.position - Player.transform.position;
+        RaycastHit hit;
+        Debug.DrawRay(Player.transform.position, Vec, Color.red);
+        if (Physics.Raycast(Player.transform.position, Vec, out hit, Mathf.Infinity))
+        {
+            Debug.Log("oui");
+            Cam.transform.position = hit.collider.transform.position;
+        }
+    }
+}
---
+++ Donjon/Assets/Scripts/Trash/Porte.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Porte : MonoBehaviour
+{
+    bool playerHere;
+    public Vector3 StartPos;
+    public Vector3 EndPos;
+    Vector3 PosIng;
+    public float duration;
+
+    private void Start()
+    {
+        StartPos = transform.position;
+    }
+
+    void Update()
+    {
+        if (playerHere)
+        {
+            OpenDoor();
+        }
+        if(!playerHere)
+        {
+            CloseDoor();
+        }
+    }
+
+    void OpenDoor()
+    {
+        PosIng = Vector3.Lerp(EndPos, StartPos, duration);
+        transform.position = PosIng;
+        Debug.Log("Open");
+    }
+    void CloseDoor()
+    {
+        PosIng = Vector3.Lerp(StartPos, EndPos, duration);
+        transform.position = PosIng;
+        Debug.Log("Close");
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerHere = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerHere = false;
+        }
+    }
+}
