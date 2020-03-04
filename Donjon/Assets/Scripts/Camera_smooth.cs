dcd9549990967c42073324ef058f5979c18c42a3
Bryndye <57522753+Bryndye@users.noreply.github.com>
2020-03-04 10:00:36 +0100

    FINISH

---
+++ Donjon/Assets/Scripts/Camera_smooth.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera_smooth : MonoBehaviour
+{
+    public float DistanceMax;
+    public float Distance;
+    public GameObject SetPositionMaxCam;
+
+    public GameObject Player;
+    public GameObject Cam;
+
+    private void Awake()
+    {
+        Player = GameObject.Find("Player");
+    }
+
+    void Update()
+    {
+        Vector3 directeur = Cam.transform.position - Player.transform.position;
+        Debug.DrawRay(Player.transform.position, directeur,Color.red ,1f);
+
+        DistanceMax = Vector3.Distance(SetPositionMaxCam.transform.position, Player.transform.position);
+
+        RaycastHit Hit;
+        if (Physics.Raycast(Player.transform.position, directeur, out Hit, Mathf.Infinity))
+        {
+            Distance = Vector3.Distance(Hit.point, Player.transform.position);
+
+            if (Distance < DistanceMax)
+            {
+                Cam.transform.position = Hit.point;
+            }
+            else if (Distance >= DistanceMax)
+            {
+                Cam.transform.position = SetPositionMaxCam.transform.position;
+            }
+        }
+
+    }
+}
+
--- Donjon/Assets/Scripts/Salle_Boss/boitier.cs
+++ Donjon/Assets/Scripts/Salle_Boss/boitier.cs
@@ -7,14 +7,28 @@ public class boitier : MonoBehaviour
     public string name;
     bool Event;
     public bool do_;
+    GameObject Player;
+    Entity entity;
+    Light light_;
+    public Color green;
+    public GameObject[] ObjectEventToActive;
 
+    private void Awake()
+    {
+        light_ = GetComponentInChildren<Light>();       
+    }
 
     void Update()
     {
-        if (Event && Input.GetButtonDown("A") && !do_)
+        if (Event && Input.GetButtonDown("A") && !do_ && entity.isPlayer)
         {
             Debug.Log("tourelle " + name + " desactive");
+            light_.color = green;
             do_ = true;
+            for (int i = 0; i < ObjectEventToActive.Length; i++)
+            {
+                ObjectEventToActive[i].SetActive(true);
+            }
         }
     }
 
@@ -23,6 +37,8 @@ public class boitier : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Event = true;
+            Player = other.gameObject;
+            entity = Player.GetComponent<Entity>();
         }
     }
     private void OnTriggerExit(Collider other)
@@ -30,6 +46,7 @@ public class boitier : MonoBehaviour
         if (other.CompareTag("Player"))
         {
             Event = false;
+            Player = null;
         }
     }
 }
